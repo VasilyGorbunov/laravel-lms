747122a7f72fe87b2795ce5d4067474c0eb5c44c
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-03-21 01:23:00 +0300

    11 - 30:00

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/courses/{course}', \App\Livewire\ShowCourse::class)->name('courses.show');
-Route::get('/courses/{course}/episodes/{episode?}', \App\Livewire\WatchEpisode::class)->name('courses.episodes.show');
+
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
